6d03c26d4bb670586ea4743c6dfedfb17273d9aa
ndd2102 <ndd.nguyenducdung@gmail.com>
2023-08-04 14:41:55 +0700

    update ip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api', // The prefix for the requests to be proxied (e.g., /api/users)
     createProxyMiddleware({
-      target: 'http://192.168.1.123:6886', // The URL of the target API server
+      target: 'http://192.168.1.32:6886', // The URL of the target API server
       changeOrigin: true, // Required for the API server to recognize the request as coming from the proxy
     })
   );
