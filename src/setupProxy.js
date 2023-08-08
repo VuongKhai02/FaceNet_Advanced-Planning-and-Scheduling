6f9986cf5c2f568cd1c8f05a31c4668c0e9d6aa7
ndd2102 <ndd.nguyenducdung@gmail.com>
2023-08-08 16:50:03 +0700

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/services/api', '/rest'],
+    ['/api', '/rest'],
     createProxyMiddleware({
       target: process.env.REACT_APP_PROXY,
       changeOrigin: true
