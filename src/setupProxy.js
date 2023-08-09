ce3c0cca3ef3a06652d2a7d2907abf6775265832
ndd2102 <ndd.nguyenducdung@gmail.com>
2023-08-09 17:26:55 +0700

    update env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/api', '/rest'],
+    ['/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/oauth2', '/login', '/auth', '/health', '/rest'],
     createProxyMiddleware({
       target: process.env.REACT_APP_PROXY,
       changeOrigin: true
