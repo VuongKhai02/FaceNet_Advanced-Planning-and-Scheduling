4820579be18cb002359fd7b6b63ffe3ec5935140
ndd2102 <ndd.nguyenducdung@gmail.com>
2023-08-12 10:21:14 +0700

    update nginx

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
-    ['/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/oauth2', '/login', '/auth', '/health', '/rest'],
-    createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY,
-      changeOrigin: true
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     ['/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/oauth2', '/login', '/auth', '/health', '/rest'],
+//     createProxyMiddleware({
+//       target: process.env.REACT_APP_PROXY,
+//       changeOrigin: true
+//     })
+//   );
+// };
