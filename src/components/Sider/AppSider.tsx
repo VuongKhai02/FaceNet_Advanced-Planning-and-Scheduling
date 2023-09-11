32793d825c5bc792d55092a52041dbe40761a0c5
tuanph111 <vuvantuandev@gmail.com>
2023-09-11 09:39:43 +0700

    update scrollbar

--- src/components/Sider/AppSider.tsx
+++ src/components/Sider/AppSider.tsx
@@ -35,30 +35,31 @@ const AppSider: React.FC<AppSiderProps> = ({ collapsed = false }) => {
             <img className={cx("logo-image")} src="src/assets/facenet-logo.svg" alt="logo" />
             {!collapsed && <img className={cx("logo-text")} src="src/assets/facenet.svg" alt="facenet" />}
         </div>
-        <Menu
-            className={cx("menu")}
-            mode="inline"
-            defaultActiveFirst
-            items={items.filter(item => !item.hidden).map(item => {
-                return {
-                    key: item.key,
-                    title: item.label,
-                    icon: item.icon,
-                    label: item.label,
-                    children: item.children?.filter(child => !child.hidden).map(child => {
-                        return {
-                            key: child.key,
-                            title: child.label,
-                            icon: child.icon,
-                            label: child.label,
-                            onClick: () => navigateTo(child.path)
-                        }
-                    }),
-                    onClick: item.children ? undefined : () => navigateTo(item.path)
-                }
-            })}
-        >
-        </Menu>
+        <div className={cx("sider-wrapper")}>
+            <Menu
+                className={cx("menu")}
+                mode="inline"
+                items={items.filter(item => !item.hidden).map(item => {
+                    return {
+                        key: item.key,
+                        title: item.label,
+                        icon: item.icon,
+                        label: item.label,
+                        children: item.children?.filter(child => !child.hidden).map(child => {
+                            return {
+                                key: child.key,
+                                title: child.label,
+                                icon: child.icon,
+                                label: child.label,
+                                onClick: () => navigateTo(child.path)
+                            }
+                        }),
+                        onClick: item.children ? undefined : () => navigateTo(item.path)
+                    }
+                })}
+            >
+            </Menu>
+        </div>
     </Sider>
 }
 
